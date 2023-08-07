4af1565b49e2ab6835eebabd1637f87ba257bfc5
nguyentronglinh045 <nguyentronglinh045@gmail.com>
2023-08-07 17:46:46 +0700

    test

--- app.js
+++ app.js
@@ -5,18 +5,18 @@ class Http {
       timeout: 10000,
       headers: {
         'Content-Type': 'application/json',
-        'expire-access-token': 5,
-        'expire-refresh-token': 10 // 30days
-      }
+        'expire-access-token': 3,
+        'expire-refresh-token': 5,
+      },
     })
-    this.requestTokenRequest = null
+    this.refreshTokenRequest = null
     this.instance.interceptors.request.use(
       (config) => {
-        console.log(config);
         const access_token = localStorage.getItem('access_token')
-        console.log('access_token in instance', access_token);
+        // console.log('access_token in instance', access_token)
         if (access_token) {
           config.headers.Authorization = access_token
+          // console.log('config', config.headers.Authorization)
         }
         return config
       },
@@ -25,24 +25,25 @@ class Http {
     this.instance.interceptors.response.use(
       (config) => config.data,
       (error) => {
-        if (
-          error.response.status === 401 &&
-          error.response.data.name === 'EXPIRED_TOKEN'
-        ) {
+        if (error.response.status === 401 && error.response.data.data.name === 'EXPIRED_TOKEN') {
+          console.log('error when refreshing token', error)
           this.refreshTokenRequest = this.refreshTokenRequest
             ? this.refreshTokenRequest
             : refreshToken().finally(() => {
-              this.refreshTokenRequest = null
-            })
+                this.refreshTokenRequest = null
+              })
+          console.log(this.refreshTokenRequest)
+
           return this.refreshTokenRequest
             .then((access_token) => {
-              error.response.config.Authorization = access_token
+              error.response.config.headers.AccessToken = access_token
               return this.instance(error.response.config)
             })
             .catch((refreshTokenerror) => {
               throw refreshTokenerror
             })
         }
+
         return Promise.reject(error)
       }
     )
@@ -58,23 +59,23 @@ class Http {
 const http = new Http()
 
 const refreshToken = async () => {
+  console.log('refreshToken func')
   const refresh_token = localStorage.getItem('refresh_token')
   try {
     const res = await http.post('refresh-access-token', {
-      refresh_token
+      refresh_token,
     })
     const { access_token } = res.data
     localStorage.setItem('access_token', access_token)
     return access_token
   } catch (error) {
-    console.log('adasd');
-    localStorage.clear()
+    console.log('Error while refreshing token:', error)
+    localStorage.clear() // Xóa local storage trong trường hợp lỗi
     throw error.response
   }
 }
 
 const fetchProfile = () => {
-  console.log('set accesstoken', localStorage.getItem('access_token'));
   http
     .get('me')
     .then((res) => {
@@ -101,13 +102,13 @@ document.getElementById('login-form').addEventListener('submit', (e) => {
   const password = document.getElementById('password').value
   http
     .post('login', {
-      "email": userName,
+      email: userName,
       password,
     })
     .then((response) => {
       localStorage.setItem('access_token', response.data.access_token)
       localStorage.setItem('refresh_token', response.data.refresh_token)
-      console.log('set accesstoken in login', localStorage.getItem('access_token'));
+      // console.log('set accesstoken in login', localStorage.getItem('access_token'))
     })
     .catch((error) => {
       console.log(error)
